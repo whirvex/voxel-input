6b02629afd397c9f1dc79aa5ca9422990b906285
Trent Summerlin <whirvis@ardenus.org>
2020-10-07 14:59:26 -0500

    Create device classes
    These classes are used to represent and handle any and all input devices. I
    have decided to use this terminology this time around, so as to avoid the
    the confusing terminology of "controller". In the original API, all input
    devices were considered controllers, even the Keyboard & Mouse. This led to
    some awkward implementations were methods such as getLeftAnalogXAxis() would
    be implemented, despite the device not even having such a peripheral.

---
+++ src/main/java/org/ardenus/input/device/DeviceType.java
@@ -0,0 +1,49 @@
+/*
+ * ___  ____ ___  ____ __   _    ___    ____ __   ____ _    ____
+ * |  \ | . \|  \ | __\| \|\|| \ | _\   |___\| \|\| . \|| \ |_ _\
+ * | . \|  <_| . \|  ]_|  \|||_|\[__ \  | /  |  \|| __/||_|\  ||
+ * |/\_/|/\_/|___/|___/|/\_/|___/|___/  |/   |/\_/|/   |___/  |/
+ *
+ * An input system created primarily for video games.
+ * Copyright (C) 2017-2020 Ardenus Studios
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+ * USA
+ */
+package org.ardenus.input.device;
+
+import java.util.Objects;
+
+public class DeviceType {
+
+	public static final DeviceType KEYBOARD = of("Keyboard & Mouse");
+	
+	public static final DeviceType XBOX = of("Microsoft XBOX One/XBOX 360 controller");
+
+	private static DeviceType of(String name) {
+		return new DeviceType(name);
+	}
+
+	private final String name;
+
+	private DeviceType(String name) throws NullPointerException {
+		this.name = Objects.requireNonNull(name, "name cannot be null");
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+}
---
+++ src/main/java/org/ardenus/input/device/InputDevice.java
@@ -0,0 +1,31 @@
+/*
+ * ___  ____ ___  ____ __   _    ___    ____ __   ____ _    ____
+ * |  \ | . \|  \ | __\| \|\|| \ | _\   |___\| \|\| . \|| \ |_ _\
+ * | . \|  <_| . \|  ]_|  \|||_|\[__ \  | /  |  \|| __/||_|\  ||
+ * |/\_/|/\_/|___/|___/|/\_/|___/|___/  |/   |/\_/|/   |___/  |/
+ *
+ * An input system created primarily for video games.
+ * Copyright (C) 2017-2020 Ardenus Studios
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+ * USA
+ */
+package org.ardenus.input.device;
+
+public interface InputDevice {
+	
+	public int getPort();
+
+}
