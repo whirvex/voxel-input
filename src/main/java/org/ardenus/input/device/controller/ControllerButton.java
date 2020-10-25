6fed5d12ad9253ba9bf4b799a57b366a1a7c477d
Trent Summerlin <whirvis@ardenus.org>
2020-10-25 01:25:22 -0500

    Add ControllerButton.java
    This is the base class for any buttons present on a controller. This slipped by
    me when getting this repo's commits up to date. Had I saw this earlier, I would
    have added both ButtonPresent and ControllerButton in a single commit. However,
    it's almost 1:30AM and I am too tired to learn how amending commits works.

---
+++ src/main/java/org/ardenus/input/device/controller/ControllerButton.java
@@ -0,0 +1,52 @@
+/*
+ * ___  ____ ___  ____ __   _    ___    ____ __   ____ _    ____
+ * |  \ | . \|  \ | __\| \|\|| \ | _\   |___\| \|\| . \|| \ |_ _\
+ * | . \|  <_| . \|  ]_|  \|||_|\[__ \  | /  |  \|| __/||_|\  ||
+ * |/\_/|/\_/|___/|___/|/\_/|___/|___/  |/   |/\_/|/   |___/  |/
+ *
+ * An input system created primarily for video games.
+ * Copyright (C) 2017-2020 Ardenus Studios
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+ * USA
+ */
+package org.ardenus.input.device.controller;
+
+import org.ardenus.input.device.DeviceButton;
+
+/**
+ * TODO
+ * 
+ * @author Trent Summerlin
+ * @since Ardenus Input v0.0.2-SNAPSHOT
+ * @version 0.0.2
+ */
+public abstract class ControllerButton extends DeviceButton {
+
+	/**
+	 * Constructs a new <code>ControllerButton</code>.
+	 * 
+	 * @param name
+	 *            the button name.
+	 * @throws NullPointerException
+	 *             if <code>name</code> or <code>controllerClazz</code> are
+	 *             <code>null</code>.
+	 */
+	public ControllerButton(String name,
+			Class<? extends Controller> controllerClazz) {
+		super(name, controllerClazz);
+	}
+
+}
